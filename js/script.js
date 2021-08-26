a812d6690d040763e518739a2dd289201e5c2973
Pepzini <52154339+Pepzini@users.noreply.github.com>
2021-08-26 12:16:34 +0100

    Design sketch

---
+++ js/script.js
@@ -0,0 +1,37 @@
+//typewriter function
+var i = 0;
+var typeWritingText = 'Welcome to 100 days of HTML, CSS and Javascript.';
+var speed = 50;
+
+function typeWriter() {
+  if (i < typeWritingText.length) {
+    document.getElementById("typewrite").innerHTML += typeWritingText.charAt(i);
+    i++;
+    setTimeout(typeWriter, speed);
+  }
+}
+typeWriter();
+//Counter.js
+const counters = document.querySelectorAll('.counter');
+
+counters.forEach(counter => {
+    //the sets the counter to 0 and overwrites what has ben written the html
+    counter.innerText = '0';
+    //this update the counter from 0 to te number you have written in you data addtribute in the html files
+    const updateCounter = () =>{
+        const target = +counter.getAttribute('data-attribute');
+
+        const updatedCounter = +counter.innerText;
+
+        const increment = target / 250;
+
+        if (updatedCounter < target) {
+            counter.innerText = Math.ceil(updatedCounter + increment);
+            setTimeout(updateCounter, 1);
+        } else{
+            counter.innerText = target;
+        }
+    }
+
+    updateCounter();
+});
