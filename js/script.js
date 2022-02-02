361f3bc9455d1fae088500f7f9021af18869ee44
Pepzini <jideniniola@gmail.com>
2022-02-03 00:21:31 +0100

    get projects by js

--- js/script.js
+++ js/script.js
@@ -21,38 +21,38 @@ async function getProjects(url) {
   const data = await response.json();
   console.log(data.projects);
   //await response.json is what returns the data
-  showProjects(data)
+  showProjects(data.projects)
 }
 function showProjects(projects) {
-  projectPage.innerText = '';
+  projectPage.innerHTML = '';
 
   projects.forEach(project => {
     const { name, imageUrl, sourceCode, demoCode } = project;
     const projectElement = document.createElement("div");
-    projectElement.classList.add("portfolio-row");
+    projectElement.classList.add("projectsCard");
 
-    projectElement = `
+    projectElement.innerHTML = `
     <div class="card">
     <img src="${imageUrl}" alt="${name}" />
     <div class="content">
       <h2>${name}</h2>
       <div class="project-buttons">
-        <button>${sourceCode}</button>
-        <button>${demoCode}</button>
+      <button> <a href="${sourceCode}">View Source Code</a> </button>
+      <button> <a href="${demoCode}">Demo</a> </button>
       </div>
     </div>`;
    projectPage.appendChild(projectElement);
   });
 }
 //snippet page function
+
 const imageContainer = document.getElementById('snippet-container');
+const imageURL = '/snippets/';
+const numberOfRows = 4;
 
-const unsplashURL = 'https://source.unsplash.com/random/';
-const numberOfRows = 5;
-
-for (let index = 0; index < numberOfRows * 4; index++) {
+for (let index = 0; index < numberOfRows * 5; index++) {
     const image = document.createElement('img');
-    image.src = `${unsplashURL}${getRandomImageSize()}`;  
+    image.src = `${imageURL}${getRandomImageSize()}`;  
     imageContainer.appendChild(image)
 }
 
@@ -61,7 +61,7 @@ function getRandomImageSize() {
 }
 function getRandomImageNumber() {
     return Math.floor(Math.random() * 10) + 300
-}
+} 
 //typewriter function
 /*var i = 0;
 var typeWritingText = 'Welcome to 100 days of HTML, CSS and Javascript.';
